ba1228f914e1a9d4fc57bb69dc0c3ffbbaeb5fa5
Léo Gouttefarde <leo.gouttefarde@ensimag.grenoble-inp.fr>
2015-05-08 03:44:29 +0200

    +

--- src/comparaisons.adb
+++ src/comparaisons.adb
@@ -6,9 +6,9 @@ package body Comparaisons is
 
 	function GenCompare(X,Y: T) return Comparaison is
 	begin
-		if X=Y then
+		if X = Y then
 			return EQ ;
-		elsif X<Y then
+		elsif X < Y then
 			return INF ;
 		else
 			return SUP ;
@@ -24,8 +24,8 @@ package body Comparaisons is
 	function Compare(S1,S2: String) return Comparaison is
 		I1,I2: Integer ;
 	begin
-		I1:=S1'First ;
-		I2:=S2'First ;
+		I1 := S1'First ;
+		I2 := S2'First ;
 		if I1 > S1'Last then
 			if I2 > S2'Last then
 				return EQ ;
@@ -36,24 +36,24 @@ package body Comparaisons is
 			return SUP ;
 		end if ;
 		loop
-			-- invariant:
+			-- Invariant :
 			--   I1 <= S1'Last et I2 <= S2'Last
 			--   et les chaines coincident jusque I1-1/I2-1.
 			if S1(I1) > S2(I2) then
 				return SUP ;
 			elsif S1(I1) < S2(I2) then
 				return INF ;
-			elsif I1=S1'Last then
+			elsif I1 = S1'Last then
 				if I2=S2'Last then
 					return EQ ;
 				else
 					return INF ;
 				end if ;
-			elsif I2=S2'Last then
+			elsif I2 = S2'Last then
 				return SUP ;
 			else
-				I1:=I1+1 ;
-				I2:=I2+1 ;
+				I1 := I1 + 1 ;
+				I2 := I2 + 1 ;
 			end if ;
 		end loop ;
 	end ;
